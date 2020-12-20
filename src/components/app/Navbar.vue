72de33e13f59ce8afff6a807ceef8baf84511af1
yanix <smirnovskoe@gmail.com>
2020-12-20 19:55:14 +0300

    Add toolbar

---
+++ src/components/app/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <v-toolbar app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span>Vuetify template</span>
+            </v-toolbar-title>
+            <v-spacer/>
+            <v-btn>
+                <span>Sign Out</span>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
